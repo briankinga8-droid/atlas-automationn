19f3dda1bc68de076e9fe34b33fa2a413ed2801e
briankinga8-droid <briankinga8@gmail.com>
2025-10-25 09:42:03 +0300

    Atlas: Added webhookRoutes to handle incoming webhook events

---
+++ routes/webhookRoutes.js
@@ -0,0 +1,9 @@
+// routes/webhookRoutes.js
+import express from "express";
+import { handleWebhook } from "../controllers/webhookController.js";
+
+const router = express.Router();
+
+router.post("/", handleWebhook);
+
+export default router;
